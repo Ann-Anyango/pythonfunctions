9b15090d85c078639640a1c96e9baa2406ca742b
Ann-Anyango <annmwangombe@gmail.com>
2023-03-29 19:34:13 +0300

    first commit

---
+++ functions/math.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    answer=a+b
+    return answer
+def subraction(c,d):
+    answer=c-d
+    return answer
+def divid(e,f):
+    answer=e/f
+    return answer
+def multiply(r,t):
+    answer=r*t
+    return answer
+def remainder(w,y):
+    answer=w%y
+    return answer
